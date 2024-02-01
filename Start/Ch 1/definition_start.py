766038f01dd594fe7f5c81fb3393bc7376a25c6b
America Ocampo <america.vianey@ymail.com>
2024-02-01 13:50:26 -0500

    OOP part 1

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,14 @@
 
 
 # TODO: create a basic class
-
+class Book:
+  def __init__(self, title):
+    self.title = title
 
 # TODO: create instances of the class
-
+book1 = Book("Bless Me Ultima")
+book2 = Book("Like Water for Chocolate")
 
 # TODO: print the class and property
+print(Book)
+print(book1.title)
